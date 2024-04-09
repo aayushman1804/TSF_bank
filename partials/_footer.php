86a1681d513455808b12f9639ee5b34804519520
aayushman1804 <147308150+aayushman1804@users.noreply.github.com>
2024-04-09 13:02:23 +0530

    add files

---
+++ partials/_footer.php
@@ -0,0 +1,4 @@
+<div class="topspace"></div>
+<footer>
+    <p>&copy 2024 Made with ❤️ By <a href="https://portfolio-aayushman.webflow.io/" target="black"><strong> Aayushman Tiwari</strong></a> | <a href="https://thesparksfoundationsingapore.org/" target="blank"><strong>The Sparks Foundation</strong></a></p>
+</footer>
